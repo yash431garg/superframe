3ab7f03d0a341eb8452fed17cad067ff088ddf4c
Yash Garg <yash431garg@gmail.com>
2024-03-15 14:19:11 +0530

    Added conditon on image at header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -12,10 +12,19 @@ export default function Header() {
         <div className={classname.header}>
             <Link href="/" className='text-2xl'>Luma Frames</Link>
             <div className={classname.headerRight}>
-                {session ? <div className='flex flex-row items-center justify-center'><Link href="/add" className='border px-4 py-2 m-4 rounded-md'>Create Frame</Link>
-                    <button onClick={() => signOut()} className="border border-gray-200 bg-gray-200 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline">
-                        Logout
-                    </button><img className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src={session?.user?.image} alt="Bordered avatar" /> </div> : <Link href="/auth">Sign in</Link>}
+                {session?.user ? (
+                    <div>
+                        <button onClick={() => signOut()} className="border border-gray-200 bg-gray-200 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline">
+                            Logout
+                        </button>
+                        {session?.user.image &&
+
+                            <img className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src={session.user.image} alt="Bordered avatar" />
+                        }
+                    </div>
+                ) : (
+                    <Link href="/auth">Sign in</Link>
+                )}
             </div>
         </div>
     )
