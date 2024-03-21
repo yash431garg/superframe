8a7b177fe8615a02b58ca6bafcfb80193f17349a
Yash Garg <yash431garg@gmail.com>
2024-03-21 20:15:33 +0530

    updated auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -11,9 +11,20 @@ export default async function SignInPage() {
     const session = await getServerSession(authConfig);
     if (session) return redirect("/");
 
+    const paragraphStyle = {
+        fontWeiht: "bold",
+        background: '-webkit-linear-gradient(#6863f8 , #ff891f )',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent'
+    };
+
     return (
         <div className="w-full flex flex-col items-center min-h-screen py-2">
+
             <div className="flex flex-col items-center mt-10 p-4 md:p-10 w-full md:w-4/12">
+                <p className="flex flex-row justify-center items-center text-center mb-5 text-4xl  italic" style={paragraphStyle}>
+                    Create a frame for you event with in just few steps.
+                </p>
                 <h1 className="mt-6 mb-2 text-2xl md:text-4xl font-bold text-center">Sign In</h1>
                 <GoogleSignInButton />
                 <span className="text-xl md:text-2xl font-semibold text-white text-center mt-6 mb-4">
@@ -21,6 +32,6 @@ export default async function SignInPage() {
                 </span>
                 <CredentialsForm />
             </div>
-        </div>
+        </div >
     );
 }
